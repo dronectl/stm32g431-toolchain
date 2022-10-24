21336e6fb39e580cbaba1e1ac4d54e3f3c59fbc4
ztnel <christian911@sympatico.ca>
2022-10-24 00:05:31 -0400

    fix gpioc moder set logic

--- src/main.c
+++ src/main.c
@@ -7,16 +7,16 @@ static void __delay(uint64_t delay) {
 
 int main(void) {
   // enable clock on port C gpio
-  RCC->AHB1ENR |= RCC_AHB2ENR_GPIOCEN;
+  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
 
   // setup pin 6 of port C as output
-  GPIOC->MODER |= GPIO_MODER_MODE6_0;
+  GPIOC->MODER &= ~(~GPIO_MODER_MODE6_0 & GPIO_MODER_MODE6_Msk);
 
   // status blinker
   while (1) {
-    GPIOC->BSRR |= (1 << 6);
-    __delay(1000);
-    GPIOC->BSRR |= (1 << (6 + 16));
-    __delay(1000);
+    GPIOC->BSRR = 1 << 6;
+    __delay(1000000);
+    GPIOC->BSRR = 1 << (6 + 16);
+    __delay(1000000);
   }
 }
